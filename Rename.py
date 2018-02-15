3b0f7591ab1854ba5fc74561a34b8c42ca0728c3
wataya <waizoo60@gmail.com>
2018-02-15 16:20:46 +0900

    20180215

--- Rename.py
+++ Rename.py
@@ -8,13 +8,24 @@ import pandas as pd
 import os
 import glob
 
-def newname(v):
+def rename(v):
     fname=glob.glob("*.tif")
     v = v.values.flatten()
-    for i in range(200):
+    for i in range(len(fname)):
         new=fname[i].split(".")[0]
         new=new+"_%02dV.tif" %v[i]
         os.rename(fname[i],new)
     os.chdir("..")
     os.chdir("..")
+    
+def get_vol():
+    d=glob.glob("*vol*.csv")[0]
+    vol = pd.read_csv(d,index_col=0)
+    return(vol)
+
+def main(directory):
+    os.chdir(directory)
+    vol = get_vol()
+    os.chdir("Stream")
+    rename(vol)
     
